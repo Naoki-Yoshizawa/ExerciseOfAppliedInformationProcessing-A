7d2197c4d98fd022c7e0cf302ed369026993d304
Naoki Yoshizawa <naoki.4438@gmail.com>
2020-02-06 15:39:14 +0900

    x,y,zに変数を変更(fix)
    変数をx,y,zに変更し連立の意味を理解しやすくした

--- Report14/SecondOrderDifferentialEquationRungeKutta.f90
+++ Report14/SecondOrderDifferentialEquationRungeKutta.f90
@@ -1,62 +1,32 @@
-program RungeKutta
+program SecondOrderRungeKutta
 !---------------------------------------------------------------
 implicit none
 integer i,div
-double precision ts,xs,zs,tf
-double precision ta,tb,za,zb,xa,xb,h
-double precision ta2,ta3,ta4,z2,z3,z4,xa2,xa3,xa4
-double precision kx1,kx2,kx3,kx4,kz1,kz2,kz3,kz4
-double precision Fx,Fz,Rungekuttafunc
+
 !---------------------------------------------------------------
-open(11,file = 'Output.txt',status = 'replace')
 
-write(*,*) 'Input ts,xs'
-read(*,*) ts,xs
-
-write(*,*) 'Input tf'
-read(*,*) tf
-
-write(*,*) 'Input divison number'
-read(*,*) div
-
-h = (tf-ts)/div
-
-ta = ts
-za = zs
-xa = xs
-
-write(11,*) ta,xa
-
-do i=1,div
-  
-end do
-
-close (11)
-
-end program RungeKutta
+end program SecondOrderRungeKutta
 
 !=========================================================================
-double precision function Fx(t,z,x)
+double precision function F(x,z)
 !---------------------------------------------------------------
 implicit none
-double precision t,z,x
+double precision a,b
+double precision x,z
 !---------------------------------------------------------------
-Fx = z
+F = -a*z-b*x
 
 return
 
 end function
 
 !=========================================================================
-double precision function Fz(t,z,x)
+double precision function G(z)
 !---------------------------------------------------------------
 implicit none
-double precision a,b
-double precision t,z,x
+double precision z
 !---------------------------------------------------------------
-a = 1
-b = 3
-Fz = -a*z-b*x
+G = z
 
 return
 
